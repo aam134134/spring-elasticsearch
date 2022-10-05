aa73bba42a07c9e750bb51fde5810c4edc3701a3
Adam Marta <oyq7@cdc.gov>
2022-10-05 17:14:08 -0400

    minor pojo update

--- src/main/java/com/example/springelasticsearch/model/Lunch.java
+++ src/main/java/com/example/springelasticsearch/model/Lunch.java
@@ -1,8 +1,6 @@
 package com.example.springelasticsearch.model;
 
 import lombok.*;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.List;
 
@@ -18,6 +16,5 @@ public class Lunch {
     @NonNull
     private String mainCourse;
 
-    @Field(type = FieldType.Nested, includeInParent = true)
     private List<String> sides;
 }
