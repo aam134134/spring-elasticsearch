0d073b7ac1394bc96df3e570f5a8309984096929
Adam Marta <oyq7@cdc.gov>
2022-10-06 17:17:18 -0400

    using native query with Spring Elastic api to deserialize objects automatically when querying

--- src/main/java/com/example/springelasticsearch/service/LunchService.java
+++ src/main/java/com/example/springelasticsearch/service/LunchService.java
@@ -1,19 +1,35 @@
 package com.example.springelasticsearch.service;
 
 import com.example.springelasticsearch.model.Lunch;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class LunchService {
 
     private final ElasticsearchOperations elasticsearchOperations;
+    private final RestHighLevelClient restHighLevelClient;
 
-    public LunchService(ElasticsearchOperations anElasticsearchOperations) {
+    public LunchService(ElasticsearchOperations anElasticsearchOperations, RestHighLevelClient aRestHighLevelClient) {
         elasticsearchOperations = anElasticsearchOperations;
+        restHighLevelClient = aRestHighLevelClient;
     }
 
     public String save(Lunch lunch) {
@@ -26,5 +42,22 @@ public class LunchService {
 
     public Lunch findById(final String lunchId) {
         return elasticsearchOperations.get(lunchId, Lunch.class, IndexCoordinates.of("lunch"));
+    }
+
+    public Map<String, Object> findByMainCourse(final String mainCourse) throws IOException {
+        SearchSourceBuilder builder = new SearchSourceBuilder().postFilter(QueryBuilders.matchQuery("mainCourse", mainCourse));
+        SearchRequest searchRequest = Requests.searchRequest("lunch");
+        searchRequest.searchType(SearchType.DFS_QUERY_THEN_FETCH);
+        searchRequest.source(builder);
+
+        SearchResponse resp = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        return resp.getHits().iterator().next().getSourceAsMap();
+    }
+
+    public SearchHits<Lunch> findByMainCourseNative(final String mainCourse) {
+        final Query q = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.matchQuery("mainCourse", mainCourse))
+                .build();
+        return elasticsearchOperations.search(q, Lunch.class, IndexCoordinates.of("lunch"));
     }
 }
--- src/test/java/com/example/springelasticsearch/SpringElasticsearchApplicationTests.java
+++ src/test/java/com/example/springelasticsearch/SpringElasticsearchApplicationTests.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
 
 @SpringBootTest
 @Testcontainers
@@ -39,5 +42,27 @@ class SpringElasticsearchApplicationTests {
         lunchService.save(new Lunch("l2", "sandwich"));
         Assertions.assertEquals("l1", lunchService.findById("l1").getLunchId());
         Assertions.assertEquals("sandwich", lunchService.findById("l2").getMainCourse());
+    }
+
+    @Test
+    void testElasticsearchRestHighLevelQuery() throws IOException {
+        lunchService.save(new Lunch("l1", "pizza"));
+        lunchService.save(new Lunch("l2", "sandwich"));
+        Assertions.assertEquals("pizza", lunchService.findByMainCourse("pizza").get("mainCourse"));
+    }
+
+    @Test
+    void testElasticsearchNativeQuery() throws IOException, InterruptedException {
+        lunchService.save(new Lunch("l3", "spaghetti"));
+        lunchService.save(new Lunch("l4", "chicken"));
+
+        // appear to be hitting a timing hole after index insert; adding delay
+        Thread.sleep(1000);
+
+        SearchHits<Lunch> hits = lunchService.findByMainCourseNative("chicken");
+        Assertions.assertEquals("l4", hits.stream().iterator().next().getContent().getLunchId());
+
+        hits = lunchService.findByMainCourseNative("pizza");
+        Assertions.assertEquals("l1", hits.stream().iterator().next().getContent().getLunchId());
     }
 }
